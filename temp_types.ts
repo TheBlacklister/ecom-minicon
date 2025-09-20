cdf33050b9a74961dc974866cb0d02b62927e1f9
siddharth654321 <siddharthrj4444@gmail.com>
2025-09-21 02:10:23 +0530

    size passed in cart page

---
+++ temp_types.ts
@@ -0,0 +1,2 @@
+failed to inspect service: request returned 500 Internal Server Error for API route and version http://%2F%2F.%2Fpipe%2FdockerDesktopLinuxEngine/v1.51/containers/supabase_db_e-commerce/json, check if the server supports the requested API version
+Try rerunning the command with --debug to troubleshoot the error.
